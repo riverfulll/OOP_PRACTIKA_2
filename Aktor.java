f5bbf0c9c5ca2b1866314bc064bdf4e9a1daef1e
Natali <natali23322332@gmail.com>
2024-04-15 19:45:10 +0300

    create aktor

---
+++ Aktor.java
@@ -0,0 +1,9 @@
+public abstract class Aktor implements AktorBehavior {
+    protected String name;
+    protected boolean isMakeOrderClass;
+    protected boolean isTakeOrderClass;
+
+    abstract String getName();
+
+
+}
